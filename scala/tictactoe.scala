99b752aad306aeb887fb40265e7af6b28a1e1df9
John Barton (joho) <jrbarton@gmail.com>
2010-12-22 19:29:59 +1100

    add a ghetto test framework to my tic tac toe game

--- scala/tictactoe.scala
+++ scala/tictactoe.scala
@@ -1,4 +1,5 @@
 class Board(val top_row: Array[String], val middle_row: Array[String], val bottom_row: Array[String]) {
+  def winner(): String = "?"
 }
 
 val x_wins_row = new Board(Array("X", "X", "X"),
@@ -17,4 +18,19 @@ val tie = new Board(Array("X", "O", "X"),
                     Array("O", "O", "X"),
                     Array("X", "X", "O"))
 
+object GameTester {
+  def test(gameName: String, board: Board, expectedWinner: String) {
+    if(board.winner() == expectedWinner) {
+      println(gameName + " passed test")
+    }
+    else {
+      println(gameName + " failed. expected " + expectedWinner + " but got " + board.winner)
+    }
+  }
+}
+
+GameTester.test("x wins row", x_wins_row, "X")
+GameTester.test("o wins col", o_wins_column, "O")
+GameTester.test("x wins diag", x_wins_diag, "X")
+GameTester.test("tie", tie, "")
 
